1a4d020f4b18ac78b48c886f477693766af2dc4f
Rajaabpro <rajaab.pro@gmail.com>
2025-08-07 16:42:01 +0500

    Update script.js to utilize math.js for mathematical operations, replacing previous command line argument handling with sum and multiplication outputs.

--- script.js
+++ script.js
@@ -1,12 +1,6 @@
-// let a = 5;
+const math = require("./math");
 
-// for (let i = 0; i < a; i++) {
-//     console.log("Hello World", i);
-// }
-
-// console.log(process.argv);
-
-let args = process.argv;
-for (let i = 2; i < args.length; i++) {
-    console.log("hello welcome to node js", args[i]);
-}
+console.log(math.sum(1, 2));
+console.log(math.mul(1, 2));
+console.log(math.g);
+console.log(math.PI);
