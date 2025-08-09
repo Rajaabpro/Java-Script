9a2c47182f2abf8d93684e370ea78dcb834e2e58
Rajaabpro <rajaab.pro@gmail.com>
2025-08-09 10:08:02 +0500

    Add routes for home, about, and contact pages in Express server; implement 404 error handling

--- ExpressDir/index.js
+++ ExpressDir/index.js
@@ -9,8 +9,36 @@ app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
 
-app.use((req, res, next) => {
-    console.log(req);
-    console.log("Request received");
-    next();
+
+app.get("/", (req, res) => {
+    res.send("Hello World");
 });
+
+app.get("/about", (req, res) => {
+    res.send("About Page");
+});
+
+app.get("/contact", (req, res) => {
+    res.send("Contact Page News and blogs");
+
+});
+
+
+app.get("*", (req, res) => {
+    res.send("404 Page Not Found");
+});
+
+// app.use((req, res) => {
+//     console.log(req.url);
+//     console.log("Request received");
+//     res.send({
+//         message: "Hello World",
+//         name: "John Doe",
+//         age: 20,
+//         email: "john.doe@example.com",
+//         phone: "1234567890",
+//         address: "123 Main St, Anytown, USA",
+//         city: "Anytown",
+//         state: "CA",
+//     });
+// });
