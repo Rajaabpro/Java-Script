1debbda096d69ff6a17d75e0a3bf93898d12ba21
Rajaabpro <rajaab.pro@gmail.com>
2025-08-11 16:57:02 +0500

    feat(index.js): add Instagram username and roll dice routes with EJS rendering

--- EJSDIR/index.js
+++ EJSDIR/index.js
@@ -29,6 +29,27 @@ app.get("/contact", (req, res) => {
     });
 });
 
+
+app.get("/ig/:username", (req, res) => {
+    const followers = ["John", "Jane", "Jim", "Jill", "Jack", "Jill", "Jim", "Jane", "John", "Jack"];
+    let {username} = req.params;
+    console.log(username);
+    res.render("insta.ejs", {
+        username: username,
+        followers: followers,
+    });
+});
+
+
+app.get("/rolldice", (req, res) => {
+    let dice = Math.floor(Math.random() * 6) + 1;
+    res.render("rolldice.ejs", {
+        title: "Roll Dice",
+        message: "Roll Dice Page",
+        dice: dice,
+    });
+});
+
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
