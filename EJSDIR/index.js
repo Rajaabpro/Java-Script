862cf373b95974bdd9015f0fff5409283f03fc2a
Rajaabpro <rajaab.pro@gmail.com>
2025-08-11 17:24:55 +0500

    refactor(index.js): comment out existing routes for home, about, contact, and rolldice while keeping Instagram route active

--- EJSDIR/index.js
+++ EJSDIR/index.js
@@ -7,42 +7,44 @@ const port = 3000;
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
-app.get("/", (req, res) => {
-    res.render("home.ejs", {
-        title: "Home",
-        message: "Hello World",
-    });
-});
+// app.get("/", (req, res) => {
+//     res.render("home.ejs", {
+//         title: "Home",
+//         message: "Hello World",
+//     });
+// });
 
-app.get("/about", (req, res) => {
-    res.render("about.ejs", {
-        title: "About",
-        message: "About Page",
-    });
-});
+// app.get("/about", (req, res) => {
+//     res.render("about.ejs", {
+//         title: "About",
+//         message: "About Page",
+//     });
+// });
 
 
-app.get("/contact", (req, res) => {
-    res.render("contact.ejs", {
-        title: "Contact",
-        message: "Contact Page",
-    });
-});
+// app.get("/contact", (req, res) => {
+//     res.render("contact.ejs", {
+//         title: "Contact",
+//         message: "Contact Page",
+//     });
+// });
 
 
 app.get("/ig/:username", (req, res) => {
+    const instaData = require("./data.json");
+    console.log(instaData);
     res.render("insta.ejs");
 });
 
 
-app.get("/rolldice", (req, res) => {
-    let dice = Math.floor(Math.random() * 6) + 1;
-    res.render("rolldice.ejs", {
-        title: "Roll Dice",
-        message: "Roll Dice Page",
-        dice: dice,
-    });
-});
+// app.get("/rolldice", (req, res) => {
+//     let dice = Math.floor(Math.random() * 6) + 1;
+//     res.render("rolldice.ejs", {
+//         title: "Roll Dice",
+//         message: "Roll Dice Page",
+//         dice: dice,
+//     });
+// });
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
