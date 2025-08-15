b396998ee1bd104efee48cacabf497511dbf6a2b
Rajaabpro <rajaab.pro@gmail.com>
2025-08-15 16:22:07 +0500

    feat(index.js): add posts array and define GET route for rendering posts in EJS template

--- REST_CLASS/index.js
+++ REST_CLASS/index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const app = express();
-
 const port = 9000;
 const path = require('path');
 
@@ -10,6 +9,25 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
+
+
+let posts = [
+    {
+        username: "Rajaab",
+        content: "Hello, world!"
+    },
+    {
+        username: "John",
+        content: "Hello, world!"
+    },
+    {
+        username: "Jane",
+        content: "Hello, world!"
+    }
+]
+app.get('/posts', (req, res) => {
+    res.render("index.ejs");
+});
 
 
 app.listen(port, () => {
