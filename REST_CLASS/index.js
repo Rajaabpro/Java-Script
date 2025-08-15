b5f447616bf2574ab95c49f73b04ec70012a01e7
Rajaabpro <rajaab.pro@gmail.com>
2025-08-15 16:07:31 +0500

    feat(index.js): configure EJS view engine and set views directory for rendering

--- REST_CLASS/index.js
+++ REST_CLASS/index.js
@@ -2,7 +2,10 @@ const express = require('express');
 const app = express();
 
 const port = 9000;
+const path = require('path');
 
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
