b1aee8b45fd3cb7859042e1e30e5b59a5d4b93c1
Rajaabpro <rajaab.pro@gmail.com>
2025-08-15 16:07:52 +0500

    added express and ejs

--- REST_CLASS/index.js
+++ REST_CLASS/index.js
@@ -4,8 +4,14 @@ const app = express();
 const port = 9000;
 const path = require('path');
 
+app.use(express.urlencoded({ extended: true }));
+
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
