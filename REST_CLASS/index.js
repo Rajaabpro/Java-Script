fa8ce7894d778d550c981c7552dfae216c4d35c7
Rajaabpro <rajaab.pro@gmail.com>
2025-08-16 11:29:12 +0500

    feat(index.js): implement CRUD operations for posts, including update and delete functionality, and enhance post content with user-specific details

--- REST_CLASS/index.js
+++ REST_CLASS/index.js
@@ -4,6 +4,7 @@ const app = express();
 const { v4: uuidv4 } = require('uuid');
 const port = 9090;
 const path = require('path');
+const methodOverride = require('method-override');
 
 app.use(express.urlencoded({ extended: true }));
 
@@ -13,22 +14,23 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(methodOverride('_method'));
 
 let posts = [
     {
         id: uuidv4(),
         username: "Rajaab",
-        content: "Hello, world!"
+        content: "Hello, world! I am Rajaab and I am a student"
     },
     {
         id: uuidv4(),
         username: "John",
-        content: "Hello, world!"
+        content: "Hello, world! I am John and I am a student"
     },
     {
         id: uuidv4(),
         username: "Jane",
-        content: "Hello, world!"
+        content: "Hello, world! I am Jane and I am a student"
     }
 ]
 
@@ -42,14 +44,38 @@ app.get('/posts/new', (req, res) => {
 
 app.post('/posts', (req, res) => {
     let { username, content } = req.body;
-    posts.push({ username, content });
+    let Id = uuidv4();
+    posts.push({ id: Id, username, content });
     res.redirect('/posts');
 });
 
 app.get('/posts/:id', (req, res) => {
     let { id } = req.params;
-    let post = posts.find(post => post.id === parseInt(id));
+    let post = posts.find(post => post.id === id);
+    if (!post) {
+        return res.status(404).render('show', { post: null });
+    }
     res.render('show', { post });
+});
+
+app.patch('/posts/:id', (req, res) => {
+    let { id } = req.params;
+    let newContent = req.body.content;
+    let post = posts.find(post => post.id === id);
+    post.content = newContent;
+    res.redirect(`/posts/${id}`);
+});
+
+app.get('/posts/:id/edit', (req, res) => {
+    let { id } = req.params;
+    let post = posts.find(post => post.id === id);
+    res.render("edit.ejs", { post });
+});
+
+app.delete('/posts/:id', (req, res) => {
+    let { id } = req.params;
+    posts = posts.filter(post => post.id !== id);
+    res.redirect('/posts');
 });
 
 app.listen(port, () => {
